ac9a175152f1e0c176bb65d53dd2341cd6a630ea
david rolfs and nick wise <david+nick@>
2017-07-03 14:15:50 -0700

    Add two way binding so the backend can trigger the front-end seamlessly.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,24 @@ import { Component } from '@angular/core';
   // template is the View
   template: `
   <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+     <h3>{{currentFocus}}</h3>
+     <ul>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
+    <hr>
+      <div>
+       <h3>{{selectedTask.description}}</h3>
+       <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+       <label>Enter Task Priority (1-3):</label>
+       <br>
+       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      </div>
   </div>
   `
 })
@@ -22,14 +35,35 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
-  ];
+    new Task('Finish weekend Angular homework for Epicodus course', 1),
+    new Task('Begin brainstorming possible JavaScript group projects',3),
+    new Task('Add README file to last few Angular repos on GitHub', 2)];
+  selectedTask: Task = this.tasks[0];
+
+  editTask(clickedTask) {
+  this.selectedTask = clickedTask;
+}
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("this task is done");
+    } else {
+      alert("this task is not done. Get to work");
+    }
+  }
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
 //Task is the Model
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public priority: number) { }
 }
