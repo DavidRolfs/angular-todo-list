0d85c9a598c855c625ec73bae10993513f7acea8
david rolfs and nick wise <david+nick@>
2017-07-03 11:55:47 -0700

    add ngFor loop, and Task Model

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,24 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template is the View
   template: `
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li>{{firstTask.description}}</li>
+      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
     </ul>
   </div>
   `
 })
 
+//AppComponent is our Component
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstTask = {
-    description: "Finish weekend Angular homework for Epicodus course"
-  }
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')
+  ];
+}
+
+//Task is the Model
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) { }
 }
